212ff7d75fcdfc9fa09fc179b29992aef5d41500
Petr Dumanskij <work.dumanskij.petr@gmail.com>
2024-01-09 21:43:20 +0500

    update dirs, download images, translit

--- src/common/functions.js
+++ src/common/functions.js
@@ -2,21 +2,27 @@ import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
 import { URL } from 'url';
-
-import { config } from '../../config/config_biefe.js';
+import CyrillicToTranslit from 'cyrillic-to-translit-js';
 
 
 export function delay(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export function mkdirIfNotExistsSync(dir) {
-	if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+export function updateDirs(siteURL, catalogueName) {
+	const DATA_DIR_NAME = CACHE.CURRENT.DATA_DIR_NAME = path.join(
+		'dist',
+		new URL(siteURL).hostname);
+	const MEDIA_DIR_NAME = CACHE.CURRENT.MEDIA_DIR_NAME = path.join(
+		DATA_DIR_NAME,
+		'media_' + catalogueName);
+
+	mkdirIfNotExistsSync(DATA_DIR_NAME, MEDIA_DIR_NAME);
 }
 
-export function getCatalogueParams(catalogueName) {
-	const CATALOGUE_NAME = path.parse(catalogueName).name;
-	const CATALOGUE = config.CATALOGUES.find(catalogue => catalogue.name === CATALOGUE_NAME);
+export function getCatalogueParams(parserName, config) {
+	const catalogueName = path.parse(parserName).name;
+	const CATALOGUE = config.CATALOGUES.find(catalogue => catalogue.name === catalogueName);
 	const ORIGIN_URL = new URL(CATALOGUE.url).origin;
 
 	return { CATALOGUE, ORIGIN_URL };
@@ -28,7 +34,7 @@ export function noArticle(rawArticle, lineNumber, url) {
 	const article = rawArticle.match(regexp)?.filter(str => !regexpExclude.test(str))[0] || rawArticle;
 
 	console.log(`Товар без артикула (строка: ${lineNumber}, вычислено: ${article}, источник: ${rawArticle}):\n${url}\n`);
-	CACHE.itemsNoArticle.add(url);
+	CACHE.itemsNoArticle.set(url, 'Нет артикула');
 
 	return article;
 }
@@ -45,15 +51,36 @@ export function formatPrice(priceString) {
 	return Math.ceil(price / n) * n;
 }
 
+export function capitalizeString(str) {
+	return str.at(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
 export function formatDescription(rawDescription) {
-	const regexp = /<(h[1-6]).*>\s*Описание\s*<\/\1>\s*/gi;
-	const description = rawDescription.replace(regexp, '').replace(/\t/g, '');
+	const regexp = /^\s*<br>\s*|<(h[1-6]).*>\s*Описание\s*<\/\1>\s*/gi;
+	const description = rawDescription.replace(regexp, '').replace(/\t/g, '').trim();
 
 	return description;
 }
 
-export async function downloadMedia(url, dirName, fileName) {
-	const filePath = path.resolve('dist', dirName, fileName);
+export function downloadImages(images, imageName, originURL = '') {
+	const imagesfileNames = [];
+	let i = 0;
+
+	for (const imageURL of images) {
+		const fileName = cyrillicToTranslit(
+			`${imageName.replace(/\//g, "-")}__${i++}` +
+			path.extname(imageURL));
+
+		imagesfileNames.push(fileName);
+		downloadMedia(originURL + imageURL, fileName);
+	}
+
+	return imagesfileNames;
+}
+
+async function downloadMedia(url, fileName) {
+	const dirName = CACHE.CURRENT.MEDIA_DIR_NAME;
+	const filePath = path.resolve(dirName, fileName);
 
 	try {
 		if (fs.existsSync(filePath)) return;
@@ -66,5 +93,15 @@ export async function downloadMedia(url, dirName, fileName) {
 		response.data.pipe(fs.createWriteStream(filePath));
 	} catch (e) {
 		console.log(`Ошибка ${downloadMedia.name} (path: ${path.join(dirName, fileName)}, url: ${url}): ${e}`);
+	}
+}
+
+function cyrillicToTranslit(str) {
+	return new CyrillicToTranslit().transform(str, '_').toLowerCase();
+}
+
+function mkdirIfNotExistsSync(...dirs) {
+	for (const dir of dirs) {
+		if (!fs.existsSync(dir)) fs.mkdirSync(dir);
 	}
 }
