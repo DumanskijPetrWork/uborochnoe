9b1427b7f1c78f154e92a7cb501b515aa2f57b35
Petr Dumanskij <work.dumanskij.petr@gmail.com>
2024-04-11 04:37:17 +0500

    disabled img's enlargement

--- config/sharp_options.js
+++ config/sharp_options.js
@@ -1,11 +1,12 @@
 export const IMG_RESIZE_OPTS = {
-	width: 1200,
+	width: 1000,
 	height: 800,
-	fit: "inside", // contain
+	fit: "inside",
+	withoutEnlargement: true,
 	background: { r: 0, g: 0, b: 0, alpha: 0 },
 };
 
 export const IMG_FORMAT_OPTS = {
-	quality: 90,
+	quality: 80,
 	extname: 'webp',
 };
