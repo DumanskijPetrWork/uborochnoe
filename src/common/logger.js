f8bc3df536211e060e9a9b477bc4f3f8e6597ba2
Petr Dumanskij <work.dumanskij.petr@gmail.com>
2024-04-11 04:38:43 +0500

    override log

--- src/common/logger.js
+++ src/common/logger.js
@@ -2,11 +2,16 @@ import fs from 'fs';
 import { Console } from 'console';
 import process from 'process';
 
-export class Logger extends Console {
+export default class Logger extends Console {
 	constructor(outputFilePath) {
 		super({
 			stdout: fs.createWriteStream(outputFilePath),
 			stderr: process.stderr
 		});
+	}
+
+	log(message, ...optionalParams) {
+		super.log(message, ...optionalParams);
+		console.log(message, ...optionalParams);
 	}
 }
