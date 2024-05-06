348b561345ac059dbbd45bcaa3df6cf89706cb46
Dumanskij <dumanskijpetr@gmail.com>
2024-05-06 03:29:47 -0700

    Full refactor

--- src/common/media.js
+++ src/common/media.js
@@ -1,64 +1,166 @@
-import axios from 'axios';
-import sharp from 'sharp';
-import fs from 'fs';
-import path from 'path';
-import { globSync } from 'glob';
-import * as f from './functions.js';
-import { IMG_FORMAT_OPTS, IMG_RESIZE_OPTS } from '../../config/sharp_options.js';
+import sharp from "sharp";
+import fs from "fs";
+import path from "path";
 
+import * as f from "./functions.js";
+import axiosHandler from "./axios.js";
+import globHandler from "./glob.js";
+import {
+	IMG_FORMAT_OPTS,
+	IMG_RESIZE_OPTS,
+} from "../../config/sharp_options.js";
 
-export function downloadImages(images, imageName, originURL = '') {
-	const imagesfileNames = [];
-	let i = 0;
+const clearImageName = function deleteInvalidFileNameSymbols(imageNameRaw) {
+	return imageNameRaw.replace(/\//g, "-").replace(/[^-\w\d\s_.]/g, "");
+};
 
-	for (const imageURL of images) {
-		if (!imageURL) continue;
+const formatImageName = f.pipe(f.cyrillicToTranslit, clearImageName);
 
-		const fileName = f.clearImageName(
-			f.cyrillicToTranslit(
-				`${imageName}__${i++}.${IMG_FORMAT_OPTS.extname}`
-			)
-		);
+const getFullNthFileName = function getFullNthFileNameWithExtension(
+	formattedImageName,
+	n,
+	extName
+) {
+	return `${formattedImageName}__${n}.${extName}`;
+};
 
-		imagesfileNames.push(fileName);
-		downloadMedia(originURL + imageURL, fileName);
-	}
-
-	return imagesfileNames;
-}
-
-async function downloadMedia(url, fileName) {
-	const dirName = CACHE.CURRENT.MEDIA_DIR_NAME;
-	const searchFilePath = path.resolve(dirName, '..', '*', fileName.replace(/\w+$/, '*'));
+const getMediaPaths = function getSearchAndNewMediaFilePaths(
+	dirName,
+	fileName
+) {
+	const filePathToSearch = path.resolve(
+		dirName,
+		"..",
+		"*",
+		fileName.replace(/\w+$/, "*")
+	);
 	const newFilePath = path.resolve(dirName, fileName);
 
-	try {
-		if (globSync(searchFilePath).length) return;
+	return { filePathToSearch, newFilePath };
+};
 
-		const { data, headers } = await axios({
+const getImageStream = async function requestImageReadableDataStreamByURL(url) {
+	const response = await axiosHandler.requestTypedContent(
+		{
 			url,
-			responseType: 'stream',
-		});
-		const contentLength = headers['content-length'];
-		const contentType = headers['content-type'];
+			responseType: "stream",
+		},
+		"image"
+	);
 
-		if (!+contentLength || !contentType.startsWith('image')) {
-			throw new Error(`Wrong file data (type: ${contentType}, length: ${contentLength})`);
+	if (!response) return;
+
+	return response.data;
+};
+
+const getTransform = function resizeAndChangeFormatPipe(onerror) {
+	const transform = sharp()
+		.resize(IMG_RESIZE_OPTS)
+		.webp(IMG_FORMAT_OPTS.quality);
+
+	transform.on("error", onerror);
+
+	return transform;
+};
+
+const transformAndSave = async function transformImageStreamDataAndSaveToFile(
+	dataStream,
+	transform,
+	filePath
+) {
+	const fileStream = fs.createWriteStream(filePath);
+
+	await dataStream.pipe(transform).pipe(fileStream);
+};
+
+/**
+ * 1. Checks if the image already exists
+ * 2. Gets the image data stream from a given URL
+ * 3. Transforms and saves the image data stream to the specified file path
+ *
+ * @param {string} imageURL - The URL of the image to download.
+ * @param {string} dirName - The directory name where the image will be saved.
+ * @param {string} fileName - The name of the image file.
+ * @param {string} [cardURL=""] - The URL of the current card.
+ * @returns {Promise<void>} - A promise that resolves when the image is successfully downloaded and saved, or rejects if an error occurs.
+ */
+const handleImage = async function checkExistanceDownloadAndSaveImage(
+	imageURL,
+	dirName,
+	fileName,
+	cardURL = ""
+) {
+	const { filePathToSearch, newFilePath } = getMediaPaths(dirName, fileName);
+
+	if (globHandler.pathExists(filePathToSearch)) return;
+
+	const transform = getTransform((e) => {
+		logger.log(
+			`Ошибка обработки файла ${fileName} (url: ${imageURL}, cardURL: ${cardURL}): ${e}`
+		);
+		fs.unlink(newFilePath, (e) => {
+			if (e) {
+				logger.log(`Ошибка удаления файла ${newFilePath}: ${e}`);
+			}
+		});
+	});
+
+	try {
+		const dataStream = await getImageStream(imageURL);
+
+		if (!dataStream) throw new Error("Изображение не получено");
+
+		await transformAndSave(dataStream, transform, newFilePath);
+	} catch (e) {
+		logger.log(
+			`Ошибка ${
+				checkExistanceDownloadAndSaveImage.name
+			} (path: ${path.join(
+				dirName,
+				fileName
+			)}, url: ${imageURL}, cardURL: ${cardURL}): ${e}`
+		);
+	}
+};
+
+/**
+ * Downloads, handles, and saves images from an array of URLs.
+ *
+ * @param {Array} imagesURLsArray - An array of image URLs.
+ * @param {string} imageNameRaw - The raw name of the image.
+ * @param {string} [cardURL=""] - The URL of the current card.
+ * @returns {string} - A string containing the names of the downloaded image files, joined by commas.
+ */
+export default function handleAndSaveImagesByURLs(
+	imagesURLsArray,
+	imageNameRaw,
+	cardURL = ""
+) {
+	const dirName = CACHE.CURRENT.MEDIA_DIR_NAME;
+	const imageName = formatImageName(imageNameRaw);
+	const extName = IMG_FORMAT_OPTS.extname;
+	const imagesFileNames = [];
+
+	imagesURLsArray.forEach((imageURL, i) => {
+		if (!imageURL || !f.isValidURL(imageURL)) {
+			logger.log(`Invalid URL: ${imageURL}`);
+
+			return;
 		}
 
-		const transform = sharp()
-			.resize(IMG_RESIZE_OPTS)
-			.webp(IMG_FORMAT_OPTS.quality)
-			.on('error', e => {
-				logger.log(`Ошибка скачивания файла ${fileName} (url: ${url}): ${e}`);
-				fs.unlinkSync(newFilePath);
-			});
-		const fileStream = fs.createWriteStream(newFilePath);
+		const fileName = getFullNthFileName(imageName, i, extName);
+		imagesFileNames.push(fileName);
+		handleImage(imageURL, dirName, fileName, cardURL);
+	});
 
-		await data
-			.pipe(transform)
-			.pipe(fileStream);
-	} catch (e) {
-		logger.log(`Ошибка ${downloadMedia.name} (path: ${path.join(dirName, fileName)}, url: ${url}): ${e}`);
+	if (imagesURLsArray.length > imagesFileNames.length) {
+		logger.log(
+			`Товар загрузил не все изображения (url: ${cardURL}, handledImages: ${imagesFileNames})`
+		);
+
+		const fileName = getFullNthFileName(imageName, 0, extName);
+		imagesFileNames.push(fileName);
 	}
+
+	return imagesFileNames.join();
 }
